223a29864b352bb2daa34ada5486484940ffe9da
codingshot <45281667+codingshot@users.noreply.github.com>
2022-12-22 22:53:25 -0500

    added code route that goes to github

--- pages/[redirect].js
+++ pages/[redirect].js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import Layout from "../components/Layout";
 
 const RedirectLinks = {
-  "/link": "",
+  "/code": "https://github.com/codingshot/near-ecosystem",
 };
 
 const Redirect = () => {
