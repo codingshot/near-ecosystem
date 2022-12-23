7288d781ac3a9e0de1023e28be0a276a1af92bf9
codingshot <45281667+codingshot@users.noreply.github.com>
2022-12-22 22:31:20 -0500

    added an embed to form

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
       <Layout pageTitle="Embed" />
       <iframe
         className="clickup-embed clickup-dynamic-height"
-        src=""
+        src="https://docs.google.com/forms/d/e/1FAIpQLSc-K4T8aB42ncEAa98XX6I5Rwt0AQ6xU4BFtQFY_WsVmP5QXw/viewform?usp=sf_link"
       ></iframe>
     </div>
   );
