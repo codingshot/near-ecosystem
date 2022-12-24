96708e5de5d9ed0804f3a18dbe207f8eb696ca66
lorem ipsumm <lorem.ipsum.crypto@gmail.com>
2022-12-24 17:46:46 -0500

    added mobile responsiveness

--- .next/static/webpack/webpack.5f613352e32a2aa7.hot-update.js
+++ .next/static/webpack/webpack.5f613352e32a2aa7.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "13221ffd843ab9e3"; }
+/******/ 	__webpack_require__.h = function() { return "ba21a0ee1ef86766"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
